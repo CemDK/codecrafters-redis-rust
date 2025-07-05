c44def9f4fae8c1b9c1176ae16b0112bc79dd594
Cem Deniz Kabakci <CemDenizKabakci@outlook.com>
2025-07-05 14:24:51 +0200

    ZU2

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,48 @@
-use std::{
-    io::{prelude::*, BufReader},
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
 };
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    loop {
+        match listener.accept().await {
+            Ok((socket, addr)) => {
+                println!("new client {:?}", addr);
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("accepted new connection");
-                handle_connection(stream);
+                tokio::spawn(async move {
+                    handle_connection(socket).await;
+                });
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+            Err(e) => println!("Couldn't get client {:?}", e),
         }
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+async fn handle_connection(mut socket: TcpStream) {
     println!("handling incoming connection");
 
-    let respone = "+PONG\r\n";
-
+    let mut buf = [0; 1024];
     loop {
-        match stream.read(&mut [0; 128]) {
+        match socket.read(&mut buf).await {
             Ok(0) => {
-                println!("No more bytes to read");
-                break;
+                println!("Connection closed by client");
+                return;
             }
-            Ok(num) => {
-                println!("Read {} bytes", num);
-                stream.write(respone.as_bytes()).unwrap();
+            Ok(n) => {
+                println!("Read {} bytes", n);
             }
             Err(e) => {
-                println!("error: {}", e);
-                break;
+                eprintln!("failed to read from socket; err = {:?}", e);
+                return;
             }
+        };
+
+        if let Err(e) = socket.write_all("+PONG\r\n".as_bytes()).await {
+            eprintln!("failed to write to socket; err = {:?}", e);
+            return;
         }
     }
 }
